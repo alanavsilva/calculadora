aaa9eb13f10a606619b24bcdf5f18b775035b93f
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-01 12:39:11 -0300

    Add- Erro de campos

--- divisao.js
+++ divisao.js
@@ -2,7 +2,15 @@ function divisao() {
     const numero = document.getElementById("numero").value;
     const numero2 = document.getElementById("numero2").value;
 
+     if ((numero == "") && (numero2 == "")) {
+        alert("Os dois campos estão vazios, não é possível realizar a conta")
+    } else if (numero == "") {
+        alert("Um dos campos está vazio, não é possível realizar a conta")
+    }else if (numero2 == "") {
+        alert("Um dos campos está vazio, não é possível realizar a conta")
+    }else{
     let divisao = Number(numero) / Number(numero2);
 
     alert("A divisão desses números é igual: " + divisao);
+}
 }
--- soma.js
+++ soma.js
@@ -2,7 +2,16 @@ function soma() {
     const numero = document.getElementById("numero").value;
     const numero2 = document.getElementById("numero2").value;
 
-    let soma = Number(numero) + Number(numero2);
+    if ((numero == "") && (numero2 == "")) {
+        alert("Os dois campos estão vazios, não é possível realizar a conta")
+    } else if (numero == "") {
+        alert("Um dos campos está vazio, não é possível realizar a conta")
+    }else if (numero2 == "") {
+        alert("Um dos campos está vazio, não é possível realizar a conta")
+    }else{
 
-    alert("A soma desses números é igual: " + soma);
+        let soma = Number(numero) + Number(numero2);
+        
+        alert("A soma desses números é igual: " + soma);
+    }
 }
