01bfcdce19804e9ef8f1098be814e1dea8db28c9
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-01 12:18:44 -0300

    Add - Subtração

--- subtracao.js
+++ subtracao.js
@@ -1,3 +1,8 @@
 function subtracao() {
+    const numero = document.getElementById("numero").value;
+    const numero2 = document.getElementById("numero2").value;
 
+    let subtracao = Number(numero) - Number(numero2);
+
+    alert("A subtração desses números é igual: " + subtracao);
 }
