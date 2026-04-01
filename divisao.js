4d03d0bfd5caa8f6206a0e4abf96c9d0312974b2
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-01 12:26:00 -0300

    Add- divisão e multiplicação

--- divisao.js
+++ divisao.js
@@ -1,3 +1,8 @@
 function divisao() {
-    
+    const numero = document.getElementById("numero").value;
+    const numero2 = document.getElementById("numero2").value;
+
+    let divisao = Number(numero) / Number(numero2);
+
+    alert("A divisão desses números é igual: " + divisao);
 }
--- multiplicacao.js
+++ multiplicacao.js
@@ -1,3 +1,8 @@
 function multiplicacao() {
-    
+    const numero = document.getElementById("numero").value;
+    const numero2 = document.getElementById("numero2").value;
+
+    let multiplicacao = Number(numero) * Number(numero2);
+
+    alert("A multiplicação desses números é igual: " + multiplicacao);
 }
