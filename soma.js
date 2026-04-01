39d5f4f291ead4990ef3e862309878d6545ce605
alana <alana_v_silva@estudante.sesisenai.org.br>
2026-04-01 12:10:00 -0300

    Add- Arrumei coisas do index

--- soma.js
+++ soma.js
@@ -1,3 +1,7 @@
 function soma() {
-    
+    const numero = el.dataset.numero;
+    const numero2 = el.dataset.numero2;
+
+    let soma = numero + numero2
+    alert("A soma desses números é igual: " + soma);
 }
